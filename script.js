6f6d5420d25b4d8f27a12394e71ddb15f3dcaf5a
Massan Sarsenbayev <100170921+msarsenbayev@users.noreply.github.com>
2023-08-19 00:27:57 +0100

    dark mode

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const toggleCheckbox = document.getElementById('toggle_checkbox');
 const body = document.body;
 
 toggleCheckbox.addEventListener('change', () => {
-    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
     updateToggleElements();
 });
 
-function updateToggleElements() {
-    const stars = document.querySelectorAll('.star');
-    const moon = document.getElementById('moon');
+// function updateToggleElements() {
+//     const stars = document.querySelectorAll('.star');
+//     const moon = document.getElementById('moon');
     
-    if (toggleCheckbox.checked) {
-        stars.forEach(star => star.style.color = '#fff'); // Change star color to white
-        moon.style.backgroundColor = '#333'; // Change moon background color
-    } else {
-        stars.forEach(star => star.style.color = '#000'); // Reset star color
-        moon.style.backgroundColor = '#f0f0f0'; // Reset moon background color
-    }
-}
+//     if (toggleCheckbox.checked) {
+//         stars.forEach(star => star.style.color = '#fff'); // Change star color to white
+//         moon.style.backgroundColor = '#333'; // Change moon background color
+//     } else {
+//         stars.forEach(star => star.style.color = '#000'); // Reset star color
+//         moon.style.backgroundColor = '#f0f0f0'; // Reset moon background color
+//     }
+// }
