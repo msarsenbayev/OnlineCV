dc126ddd13cc2525e8067f1346d930f65d25df41
Massan Sarsenbayev <100170921+msarsenbayev@users.noreply.github.com>
2023-10-06 21:42:13 +0100

    toggle fixed

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
+// const toggleCheckbox = document.getElementById('toggle_checkbox');
+// const body = document.body;
+
+// let userToggled = false; // Track user's toggle action
+
+// // Retrieve the theme preference from localStorage
+// const savedTheme = localStorage.getItem('theme');
+
+// // Set the initial theme based on the saved preference
+// if (savedTheme === 'light') {
+//     body.classList.add('light-mode');
+//     updateToggleElements(true);
+// } else {
+//     updateToggleElements(false);
+// }
+
+// toggleCheckbox.addEventListener('change', () => {
+//     body.classList.toggle('light-mode');
+//     updateToggleElements(body.classList.contains('light-mode'));
+
+//     // Save the theme preference to localStorage
+//     const themeToSave = body.classList.contains('light-mode') ? 'light' : 'dark';
+//     localStorage.setItem('theme', themeToSave);
+// });
+
+
+// function updateToggleElements(isDarkMode) {
+//     const stars = document.querySelectorAll('.star');
+//     const moon = document.getElementById('moon');
+    
+//     if (isDarkMode) {
+//         stars.forEach(star => star.style.color = '#fff');
+//         moon.style.backgroundColor = '#333';
+//     } else {
+//         stars.forEach(star => star.style.color = '#000');
+//         moon.style.backgroundColor = '#f0f0f0';
+//     }
+// }
+
+
 const toggleCheckbox = document.getElementById('toggle_checkbox');
 const body = document.body;
-
-let userToggled = false; // Track user's toggle action
 
 // Retrieve the theme preference from localStorage
 const savedTheme = localStorage.getItem('theme');
 
-// Set the initial theme based on the saved preference
-if (savedTheme === 'light') {
-    body.classList.add('light-mode');
-    updateToggleElements(true);
-} else {
-    updateToggleElements(false);
+// Function to set the theme based on the saved preference
+function setTheme(theme) {
+    if (theme === 'light') {
+        body.classList.add('light-mode');
+        updateToggleElements(true);
+        toggleCheckbox.checked = true; // Update the checkbox state
+    } else {
+        updateToggleElements(false);
+        toggleCheckbox.checked = false; // Update the checkbox state
+    }
 }
+
+// Set the initial theme based on the saved preference
+setTheme(savedTheme);
 
 toggleCheckbox.addEventListener('change', () => {
     body.classList.toggle('light-mode');
@@ -23,11 +68,10 @@ toggleCheckbox.addEventListener('change', () => {
     localStorage.setItem('theme', themeToSave);
 });
 
-
 function updateToggleElements(isDarkMode) {
     const stars = document.querySelectorAll('.star');
     const moon = document.getElementById('moon');
-    
+
     if (isDarkMode) {
         stars.forEach(star => star.style.color = '#fff');
         moon.style.backgroundColor = '#333';
